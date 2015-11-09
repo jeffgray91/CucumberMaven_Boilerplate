470a7e02c26098a0cfb7c293f3bb5cc6794f2982
jeffgrayroundingwell <jeff.gray@roundingwell.com>
2015-11-09 13:22:53 -0600

    Added Safari Driver

--- src/test/java/AppName/Steps/OpenBrowserHelp.java
+++ src/test/java/AppName/Steps/OpenBrowserHelp.java
@@ -2,7 +2,7 @@ package AppName.Steps;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.safari.SafariDriver;
 import java.util.concurrent.TimeUnit;
 
 public class OpenBrowserHelp {
@@ -11,8 +11,10 @@ public class OpenBrowserHelp {
     private static OpenBrowserHelp browserHelp;
 
     private OpenBrowserHelp() {
-        //add this if using the chrome driver
-       // System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        // add this if using the chrome driver
+        // System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        // Enable this for safari
+        // System.setProperty("webdriver.chrome.driver","src/test/resources/safaridriver");
         this.driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://www.wikipedia.org");
